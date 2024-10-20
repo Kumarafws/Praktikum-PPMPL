57b78607f076ddb62ec2de495535afbcb1c4cb9b
Kumarafws <kumarafawwas@gmail.com>
2024-10-20 16:20:19 +0700

    Praktikum 3

---
+++ src/app.js
@@ -0,0 +1,23 @@
+// Kumara Fawwas Abhista
+// 2200016100
+// Kelas C
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const controller = require('./controller');
+
+app.use(express.json());
+
+
+app.get('/api/items', controller.getItems);  
+app.post('/api/items', controller.createItem); 
+app.put('/api/items/:id', controller.updateItem); 
+app.delete('/api/items/:id', controller.deleteItem); 
+
+app.listen(port, () => { 
+    console.log(`API is running on http://localhost:${port}`);
+});
+
+module.exports = app;
